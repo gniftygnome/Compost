bc8c276a100324fa5d9451e2ad7768434aa6c976
yurisuika <yurisuika@protonmail.com>
2022-06-14 23:58:24 -0700

    SLF4J Again

--- src/main/java/com/yurisuika/compost/Compost.java
+++ src/main/java/com/yurisuika/compost/Compost.java
@@ -1,16 +1,16 @@
 package com.yurisuika.compost;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 @Mod("compost")
 public class Compost
 {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public Compost()
     {
