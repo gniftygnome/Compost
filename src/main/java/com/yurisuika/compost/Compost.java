038a964980dce2362d465a0dee1929006bf18727
yurisuika <yurisuika@protonmail.com>
2022-12-07 03:44:56 -0800

    Made JSON Configurable

--- src/main/java/com/yurisuika/compost/Compost.java
+++ src/main/java/com/yurisuika/compost/Compost.java
@@ -1,27 +1,75 @@
 package com.yurisuika.compost;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 
 @Mod("compost")
 public class Compost
 {
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    public static File file = new File(FMLPaths.CONFIGDIR.get().toFile(), "compost.json");
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static CompostConfig config = new CompostConfig();
+
+    public static void saveConfig() {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(getConfig()));
+            fileWriter.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void loadConfig() {
+        try {
+            if (file.exists()) {
+                config = gson.fromJson(Files.readString(file.toPath()), CompostConfig.class);
+            } else {
+                config = new CompostConfig();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setConfig(config);
+    }
+
+    public static void setConfig(CompostConfig config) {
+        Compost.config = config;
+    }
+
+    public static CompostConfig getConfig() {
+        return config;
+    }
+
     public Compost()
     {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event)
     {
         LOGGER.info("Loading Compost!");
+
+        if (!file.exists()) {
+            saveConfig();
+        }
+        loadConfig();
     }
 
 }
--- src/main/java/com/yurisuika/compost/mixin/world/level/block/ComposterBlockMixin.java
+++ src/main/java/com/yurisuika/compost/mixin/world/level/block/ComposterBlockMixin.java
@@ -1,24 +1,44 @@
 package com.yurisuika.compost.mixin.world.level.block;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
+import com.yurisuika.compost.Compost;
+import com.yurisuika.compost.CompostConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ComposterBlock;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Mixin(ComposterBlock.class)
 public class ComposterBlockMixin {
 
-    @Redirect(method = "extractProduce", at = @At(value = "FIELD", target = "Lnet/minecraft/world/item/Items;BONE_MEAL:Lnet/minecraft/world/item/Item;", opcode = Opcodes.GETSTATIC))
-    private static Item redirectEmptyFullComposter() {
-        return Items.DIRT;
+    @Redirect(method = "extractProduce", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
+    private static boolean redirectExtractProduce(Level level, Entity entity) {
+        return false;
     }
 
-    @Redirect(method = "getContainer", at = @At(value = "FIELD", target = "Lnet/minecraft/world/item/Items;BONE_MEAL:Lnet/minecraft/world/item/Item;", opcode = Opcodes.GETSTATIC))
-    private Item redirectGetInventory() {
-        return Items.DIRT;
+    @Inject(method = "extractProduce", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z", shift = At.Shift.AFTER))
+    private static void injectExtractProduce(BlockState state, Level level, BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
+        for (CompostConfig.Group group : Compost.config.items) {
+            double d = (double)(level.random.nextFloat() * 0.7F) + 0.15000000596046448D;
+            double e = (double)(level.random.nextFloat() * 0.7F) + 0.06000000238418579D + 0.6D;
+            double g = (double)(level.random.nextFloat() * 0.7F) + 0.15000000596046448D;
+            ItemEntity itemEntity = new ItemEntity(level, (double)pos.getX() + d, (double)pos.getY() + e, (double)pos.getZ() + g, new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(group.item)), ThreadLocalRandom.current().nextInt(group.min, group.max + 1)));
+            itemEntity.setDefaultPickUpDelay();
+            if(ThreadLocalRandom.current().nextFloat() < group.chance) {
+                level.addFreshEntity(itemEntity);
+            }
+        }
     }
 
 }
