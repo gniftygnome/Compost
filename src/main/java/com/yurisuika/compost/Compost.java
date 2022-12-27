f5704786897a76f2d98a4253543dae65d5fd6321
yurisuika <yurisuika@protonmail.com>
2022-12-26 17:32:29 -0800

    Backport 1.1.1

--- src/main/java/com/yurisuika/compost/Compost.java
+++ src/main/java/com/yurisuika/compost/Compost.java
@@ -2,14 +2,21 @@ package com.yurisuika.compost;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.yurisuika.compost.server.command.CompostCommand;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 public class Compost implements ModInitializer {
 
@@ -18,7 +25,18 @@ public class Compost implements ModInitializer {
     public static File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "compost.json");
     public static Gson gson = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting().create();
 
-    public static CompostConfig config = new CompostConfig();
+    public static Config config = new Config();
+
+    public static class Config {
+
+        public boolean shuffle = true;
+
+        public Group[] items = {
+                new Group("minecraft:dirt", 1.0D, 1,1),
+                new Group("minecraft:bone_meal", 1.0D, 1, 1)
+        };
+
+    }
 
     public static void saveConfig() {
         try {
@@ -30,25 +48,100 @@ public class Compost implements ModInitializer {
         }
     }
 
-    static void loadConfig() {
+    public static void loadConfig() {
         try {
             if (file.exists()) {
-                config = gson.fromJson(Files.readString(file.toPath()), CompostConfig.class);
+                config = gson.fromJson(Files.readString(file.toPath()), Config.class);
             } else {
-                config = new CompostConfig();
+                config = new Config();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        checkBounds();
         setConfig(config);
     }
 
-    public static void setConfig(CompostConfig config) {
+    public static void setConfig(Config config) {
         Compost.config = config;
     }
 
-    public static CompostConfig getConfig() {
+    public static Config getConfig() {
         return config;
+    }
+
+    public static void checkBounds() {
+        Arrays.stream(Compost.config.items).forEach(group -> {
+            Item item;
+            int index;
+            if (group.item.contains("{")) {
+                index = group.item.indexOf("{");
+                String id = group.item.substring(0, index);
+                item = Registry.ITEM.get(new Identifier(id));
+            } else {
+                item = Registry.ITEM.get(new Identifier(group.item));
+            }
+            group.chance = Math.max(0.0D, Math.min(group.chance, 1.0D));
+            int maxCount = item.getMaxCount();
+            group.max = Math.min(group.max, maxCount);
+            group.min = Math.min(Math.min(group.min, maxCount), group.max);
+        });
+        saveConfig();
+    }
+
+    public static void setShuffle(boolean bool) {
+        config.shuffle = bool;
+        saveConfig();
+    }
+
+    public static void setGroup(int group, String item, double chance, int min, int max) {
+        config.items[group] = new Group(item, chance, min, max);
+        saveConfig();
+    }
+
+    public static Group getGroup(int group) {
+        return ArrayUtils.get(config.items, group);
+    }
+
+    public static void addGroup(String item, double chance, int min, int max) {
+        config.items = ArrayUtils.add(config.items, new Group(item, chance, min, max));
+        saveConfig();
+    }
+
+    public static void insertGroup(int group, String item, double chance, int min, int max) {
+        config.items = ArrayUtils.insert(group, config.items, new Group(item, chance, min, max));
+        saveConfig();
+    }
+
+    public static void removeGroup(int group) {
+        config.items = ArrayUtils.remove(config.items, group);
+        saveConfig();
+    }
+
+    public static void reverseGroups() {
+        ArrayUtils.reverse(config.items);
+        saveConfig();
+    }
+
+    public static void shuffleGroups() {
+        ArrayUtils.shuffle(config.items);
+        saveConfig();
+    }
+
+    public static class Group {
+
+        public String item;
+        public double chance;
+        public int min;
+        public int max;
+
+        Group(String item, double chance, int min, int max) {
+            this.item = item;
+            this.chance = chance;
+            this.min = min;
+            this.max = max;
+        }
+
     }
 
     @Override
@@ -59,6 +152,8 @@ public class Compost implements ModInitializer {
             saveConfig();
         }
         loadConfig();
+
+        CommandRegistrationCallback.EVENT.register(CompostCommand::register);
     }
 
 }
--- src/main/java/com/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/com/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -1,39 +1,36 @@
 package com.yurisuika.compost.integration.roughlyenoughitems;
 
-import com.google.common.collect.Iterators;
-import com.yurisuika.compost.Compost;
-import com.yurisuika.compost.CompostConfig;
-import it.unimi.dsi.fastutil.objects.Object2FloatMap;
-import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
-import me.shedaniel.rei.api.common.util.EntryIngredients;
-import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.block.ComposterBlock;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+        import com.google.common.collect.Iterators;
+        import com.yurisuika.compost.Compost;
+        import it.unimi.dsi.fastutil.objects.Object2FloatMap;
+        import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+        import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+        import me.shedaniel.rei.api.common.util.EntryIngredients;
+        import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
+        import net.fabricmc.api.EnvType;
+        import net.fabricmc.api.Environment;
+        import net.minecraft.block.ComposterBlock;
+        import net.minecraft.item.ItemConvertible;
+        import net.minecraft.item.ItemStack;
+        import net.minecraft.util.Identifier;
+        import net.minecraft.util.registry.Registry;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+        import java.util.*;
 
 @Environment(EnvType.CLIENT)
 public class CompostClientPlugin implements REIClientPlugin {
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
-        for (CompostConfig.Group group : Compost.config.items) {
+        Arrays.stream(Compost.config.items).forEach(group -> {
             int page = 0;
             Iterator<List<Object2FloatMap.Entry<ItemConvertible>>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).iterator(), 35);
             while (iterator.hasNext()) {
                 List<Object2FloatMap.Entry<ItemConvertible>> entries = iterator.next();
                 registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(new ItemStack(Registry.ITEM.get(new Identifier(group.item))))), page++));
             }
-        }
+        });
     }
 
 }
+
