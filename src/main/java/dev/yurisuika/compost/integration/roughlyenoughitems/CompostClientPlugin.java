2cc9c7c0ac6f94414888a29f23ffa4a2e8a6a4ae
yurisuika <yurisuika@protonmail.com>
2023-02-05 23:54:24 -0800

    Migrate to Yarn + Loom

--- src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -8,11 +8,11 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.forge.REIPluginClient;
 import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.block.ComposterBlock;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.*;
 
@@ -23,10 +23,10 @@ public class CompostClientPlugin implements REIClientPlugin {
     public void registerDisplays(DisplayRegistry registry) {
         Arrays.stream(Compost.config.items).forEach(group -> {
             int page = 0;
-            Iterator<List<Object2FloatMap.Entry<ItemLike>>> iterator = Iterators.partition(ComposterBlock.COMPOSTABLES.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).iterator(), 35);
+            Iterator<List<Object2FloatMap.Entry<ItemConvertible>>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).iterator(), 35);
             while (iterator.hasNext()) {
-                List<Object2FloatMap.Entry<ItemLike>> entries = iterator.next();
-                registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(new ItemStack(Registry.ITEM.get(new ResourceLocation(group.item))))), page++));
+                List<Object2FloatMap.Entry<ItemConvertible>> entries = iterator.next();
+                registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(new ItemStack(ForgeRegistries.ITEMS.getValue(new Identifier(group.item))))), page++));
             }
         });
     }
