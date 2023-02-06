5667275bd6cdf1c3a7f52af48d52dbdd990394ba
yurisuika <yurisuika@protonmail.com>
2023-02-06 00:47:33 -0800

    Fixed REI plugin when item has NBT

--- src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -1,6 +1,7 @@
 package dev.yurisuika.compost.integration.roughlyenoughitems;
 
         import com.google.common.collect.Iterators;
+        import com.mojang.brigadier.exceptions.CommandSyntaxException;
         import dev.yurisuika.compost.Compost;
         import it.unimi.dsi.fastutil.objects.Object2FloatMap;
         import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
@@ -10,8 +11,11 @@ package dev.yurisuika.compost.integration.roughlyenoughitems;
         import net.fabricmc.api.EnvType;
         import net.fabricmc.api.Environment;
         import net.minecraft.block.ComposterBlock;
+        import net.minecraft.item.Item;
         import net.minecraft.item.ItemConvertible;
         import net.minecraft.item.ItemStack;
+        import net.minecraft.nbt.NbtCompound;
+        import net.minecraft.nbt.StringNbtReader;
         import net.minecraft.util.Identifier;
         import net.minecraft.util.registry.Registry;
 
@@ -26,8 +30,27 @@ public class CompostClientPlugin implements REIClientPlugin {
             int page = 0;
             Iterator<List<Object2FloatMap.Entry<ItemConvertible>>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).iterator(), 35);
             while (iterator.hasNext()) {
+                int index;
+                Item item;
+                if (group.item.contains("{")) {
+                    index = group.item.indexOf("{");
+                    item = Registry.ITEM.get(new Identifier(group.item.substring(0, index)));
+                } else {
+                    index = 0;
+                    item = Registry.ITEM.get(new Identifier(group.item));
+                }
+                ItemStack itemStack = new ItemStack(item);
+                if (group.item.contains("{")) {
+                    NbtCompound nbt;
+                    try {
+                        nbt = StringNbtReader.parse(group.item.substring(index));
+                        itemStack.setNbt(nbt);
+                    } catch (CommandSyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
                 List<Object2FloatMap.Entry<ItemConvertible>> entries = iterator.next();
-                registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(new ItemStack(Registry.ITEM.get(new Identifier(group.item))))), page++));
+                registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(itemStack)), page++));
             }
         });
     }
