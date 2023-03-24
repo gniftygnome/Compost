6ceca1aa37fcc5b1d29f2c454b73be6352d8bb80
yurisuika <yurisuika@protonmail.com>
2023-03-24 11:02:20 -0700

    Registered Forge style

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -17,7 +17,11 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.File;
@@ -139,11 +143,9 @@ public class Compost {
         saveConfig();
     }
 
-    public static BlockEntityType<ComposterBlockEntity> COMPOSTER = Registry.register(
-            Registry.BLOCK_ENTITY_TYPE,
-            new Identifier("compost", "composter"),
-            BlockEntityType.Builder.create(ComposterBlockEntity::new, Blocks.COMPOSTER).build(null)
-    );
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, "compost");
+
+    public static final RegistryObject<BlockEntityType<ComposterBlockEntity>> COMPOSTER = BLOCK_ENTITIES.register("composter", () -> BlockEntityType.Builder.create(ComposterBlockEntity::new, Blocks.COMPOSTER).build(null));
 
     @Mod.EventBusSubscriber(modid = "compost")
     public static class CommonForgeEvents {
@@ -162,6 +164,8 @@ public class Compost {
         loadConfig();
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        BLOCK_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
 }
