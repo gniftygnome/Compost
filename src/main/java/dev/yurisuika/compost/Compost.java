d85cde573fab24d2cd0f4bb7b6b4055aa32e99a6
yurisuika <yurisuika@protonmail.com>
2023-03-24 09:33:50 -0700

    Block entity composter port

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -3,7 +3,10 @@ package dev.yurisuika.compost;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.yurisuika.compost.block.entity.ComposterBlockEntity;
 import dev.yurisuika.compost.server.command.CompostCommand;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
@@ -19,10 +22,8 @@ import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Mod("compost")
@@ -33,8 +34,6 @@ public class Compost {
     public static Config config = new Config();
 
     public static class Config {
-
-        public boolean shuffle = true;
 
         public Group[] items = {
             new Group("minecraft:dirt", 1.0D, 1, 1),
@@ -126,16 +125,6 @@ public class Compost {
         return itemStack;
     }
 
-    public static void setShuffle(boolean bool) {
-        config.shuffle = bool;
-        saveConfig();
-    }
-
-    public static void setGroup(int group, String item, double chance, int min, int max) {
-        config.items[group] = new Group(item, chance, min, max);
-        saveConfig();
-    }
-
     public static Group getGroup(int group) {
         return config.items[group];
     }
@@ -145,35 +134,16 @@ public class Compost {
         saveConfig();
     }
 
-    public static void insertGroup(int group, String item, double chance, int min, int max) {
-        Group[] array = (Group[]) Array.newInstance(config.items.getClass().getComponentType(), config.items.length + 1);
-        System.arraycopy(new Group[]{new Group(item, chance, min, max)}, 0, array, group, 1);
-        if (group > 0) {
-            System.arraycopy(config.items, 0, array, 0, group);
-        }
-        if (group < config.items.length) {
-            System.arraycopy(config.items, group, array, group + 1, config.items.length - group);
-        }
-        config.items = array;
-        saveConfig();
-    }
-
     public static void removeGroup(int group) {
         config.items = ArrayUtils.remove(config.items, group);
         saveConfig();
     }
 
-    public static void reverseGroups() {
-        ArrayUtils.reverse(config.items);
-        saveConfig();
-    }
-
-    public static void shuffleGroups(Random random) {
-        for (int i = config.items.length; i > 1; i--) {
-            ArrayUtils.swap(config.items, i - 1, random.nextInt(i), 1);
-        }
-        saveConfig();
-    }
+    public static BlockEntityType<ComposterBlockEntity> COMPOSTER = Registry.register(
+            Registry.BLOCK_ENTITY_TYPE,
+            new Identifier("compost", "composter"),
+            BlockEntityType.Builder.create(ComposterBlockEntity::new, Blocks.COMPOSTER).build(null)
+    );
 
     @Mod.EventBusSubscriber(modid = "compost")
     public static class CommonForgeEvents {
--- src/main/java/dev/yurisuika/compost/mixin/block/ComposterBlockInvoker.java
+++ src/main/java/dev/yurisuika/compost/mixin/block/ComposterBlockInvoker.java
@@ -2,6 +2,7 @@ package dev.yurisuika.compost.mixin.block;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ComposterBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,8 +11,8 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(ComposterBlock.class)
 public interface ComposterBlockInvoker {
 
-    @Invoker("emptyComposter")
-    static BlockState invokeEmptyComposter(BlockState state, WorldAccess world, BlockPos pos) {
+    @Invoker("addToComposter")
+    static BlockState invokeAddToComposter(BlockState state, WorldAccess world, BlockPos pos, ItemStack itemStack) {
         throw new AssertionError();
     }
 
